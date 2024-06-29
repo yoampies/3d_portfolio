7c1255b44349ab67acd33d78fbaf3b27a9a992a7
yoampies <andresampies@gmail.com>
2024-06-28 21:08:49 -0400

    started threejs desktop 2.0

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {motion} from 'framer-motion';
+import {styles} from '../styles';
+import { ComputersCanvas} from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+          <div className='w-1 sm:h-80 h-36 violet-gradient'/>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'> Andrés</span></h1>
+          <p className={`${styles.heroSubtext} mt-2 text-white-100`}>Proficient Web Developer experienced in creating role-based 
+            platforms and database management. Leveraging UI/UX design principles and a diverse tech stack, including Laravel and 
+            React JS, to successfully optimize development strategies.</p>
+        </div>
+      </div>
+      
+      <ComputersCanvas />
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
-          <p className='text-white text-[18px] cursor-pointer flex'>Andrés Ampíes &nbsp; <span className='sm:block hidden'>| Web Developer</span></p>
+          <p className='text-white text-[18px] cursor-pointer flex'>Andrés Ampíes &nbsp; <span className='lg:flex hidden'>| Web Developer</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
